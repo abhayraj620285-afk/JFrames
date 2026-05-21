e2696f4b2d2f3344f3e75ee54d4d2810100182c2
Abhay Raj <abhayraj620285@gmail.com>
2026-05-22 01:35:32 +0530

    learned frame

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     static void main(){
 
       // JFrame = a GUL window to add components
-        JFrame frame = new JFrame(); // create a frame
+//        JFrame frame = new JFrame(); // create a frame
+//
+//        frame.setTitle("JFrame title goes here");
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of application
+//        frame.setResizable(false); // prevent frames from resizing
+//        frame.setSize(500, 500); // Sets the x dimension,y dimension for the frames
+//
+//        ImageIcon image = new ImageIcon("src/images/apple-logo.png");
+//        frame.setIconImage(image.getImage());
+//        frame.setVisible(true); // makes frames visible
+//        frame.getContentPane().setBackground(new Color(123,50,250)); // change color of background
 
-
-        frame.setSize(500, 500);
-
-        frame.setVisible(true);
-
-
-
+        MyFrame myFrame = new MyFrame(); // Second Method for creating frame
     }
+
 }
