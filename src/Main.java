39deef4a138e0860e87b4235e87517e4e142d5ab
Abhay Raj <abhayraj620285@gmail.com>
2026-05-22 00:36:03 +0530

    added main

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+public class Main {
+    static void main(){
+
+      // JFrame = a GUL window to add components
+        JFrame frame = new JFrame(); // create a frame
+
+
+        frame.setSize(500, 500);
+
+        frame.setVisible(true);
+
+
+
+    }
+}
